b6f7c88bd028ae69ebaf89b93995290b816544ab
Abhishek Chavan <abhi32chavan@gmail.com>
2026-02-13 22:29:37 +0530

    blogs by categories

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,7 +1,16 @@
 from   django.http import HttpResponse
 from django.shortcuts import render 
-
+from blogs.models import Blog, category
 
 def home(request):
     # return HttpResponse("<h1> Home Page <h1>")
-    return render(request,'home.html')
+    categories = category.objects.all()
+    featured_posts =Blog.objects.filter(is_featured=True,status="Published").order_by("updated_at")
+    posts =Blog.objects.filter(is_featured=False,status="Published")
+
+    context ={ 
+        'categories':categories,
+        'featured_posts':featured_posts,
+        'posts':posts,
+    }
+    return render(request,'home.html',context)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from .models import Blog,category
 
 # Create your views here.
+
+def posts_by_category(request,category_id):
+
+    posts=Blog.objects.filter(status="Published",category=category_id)
+    try:
+        Category = category.objects.get(pk=category_id)
+    except:
+        return redirect('home')
+    context ={
+        "posts": posts,
+        "category":Category,    
+    }
+    return render(request,'posts_by_category.html',context) 
